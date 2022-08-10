15d9beb89b59351af88e8ac1ef9360167951202c
kkkkkn <1185553557@qq.com>
2022-08-10 17:56:46 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:kreader/app.dart';
+import 'package:kreader/pages/login.dart';
 import 'package:kreader/pages/recommend.dart';
 
 
 void main() {
-  runApp(const App());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'flutter demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      routes: {
+        '/':(BuildContext context)=>const Login(),
+        '/home':(BuildContext context)=>const App(),
+        '/login':(BuildContext context)=>const Login(),
+      },
+    );
+  }
+  
 }
